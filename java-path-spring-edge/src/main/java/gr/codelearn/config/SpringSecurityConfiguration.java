b26194b6293f21ca1700bf132f39222219020e7d
WineCellarZeus <pkavvadias@athtech.gr>
2021-12-21 13:59:49 +0200

    3rd try

---
+++ java-path-spring-edge/src/main/java/gr/codelearn/config/SpringSecurityConfiguration.java
@@ -0,0 +1,46 @@
+package gr.codelearn.config;
+
+import gr.codelearn.service.CustomUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SpringSecurityConfiguration extends WebSecurityConfigurerAdapter{
+
+    //Basic Auth
+
+    @Autowired
+    CustomUserDetailsService userDetailsService;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    public void configure(AuthenticationManagerBuilder auth) throws Exception
+    {
+        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+    }
+
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        // We don't need CSRF for this example
+        httpSecurity.csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/api/feeder")
+                .hasAnyRole("USER")
+                .antMatchers("/api/wallet/feeder")
+                .hasAnyRole("USER")
+                .and().httpBasic();
+    }
+
+}
